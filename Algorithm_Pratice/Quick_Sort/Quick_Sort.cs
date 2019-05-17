344cb693be9fb7deeef3144d14963f3555ad78e2
cloude19 <troycloude@gmail.com>
2019-05-16 23:05:51 -0400

    chages made to Quick Sort position method.

---
+++ Algorithm_Pratice/Quick_Sort/Quick_Sort.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quick_Sort
+{
+    static class Quick_Sort
+    {
+        static Quick_Sort()
+        {
+        }
+
+        static void Sort(params int[] Values)
+        {
+            //assign pivot
+           var pivot = CPosition(Values);
+
+            //check right and left of pivot recursivly
+
+        }
+
+        static int CPosition(int[] Values)
+        {
+
+            return 1;
+        }
+    }
+}
